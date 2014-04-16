308802dba5ae27a691931da42ed44ae26489f3e7
Tyler Hannan <thannan@novetta.com>
2014-04-15 21:42:04 -0400

    Instructor DAL first commit.

---
+++ Instructor.php
@@ -0,0 +1,60 @@
+<?php
+
+
+require_once 'dbsetup.php';
+
+
+
+function getMapping($sql,$arr,$callback) {
+        global $db;
+        $stmt = $db->prepare($sql);
+        $stmt->execute($arr);
+        $stmt->setFetchMode(PDO::FETCH_NUM);
+        return array_map($callback,$stmt);
+}
+
+class Instructor
+{
+        private $netid;
+        private $name;
+        private $email;
+        private $office;
+        private $classes;
+
+        public function __construct($row) {
+                $this->netid = $row['netid'];
+                $this->name = $row['name'];
+                $this->office = $row['office'];
+                if (array_key_exists('email',$row))
+                        $this->email = $row['email'];
+                else
+                        $this->email = $row['name'] . "@u.rochester.edu";
+                $this->classes = NULL;
+        }
+
+        public function getNetID() {return $this->netid;}
+        public function getName()  {return $this->name;}
+        public function getEmail() {return $this->email;}
+        public function getOffice() {return $this->office;}
+
+        public function getClasses() {
+                if ($this->classes) return $this->classes;
+                return getMapping("SELECT * FROM Teaches INNER JOIN Courses
+                        ON Teaches.course = Courses.id
+                        WHERE
+                        Teaches.instructor = :netid",
+                        array(':netid' => $this->netid),
+                        function ($x) { return Course($x); });
+        }
+
+        static public function getByNetID($netid) {
+                global $db;
+                $stmt = $db->prepare('SELECT * FROM Instructors WHERE netid=:netid;');
+                $stmt->execute(array(':netid' => $netid));
+                $stmt->setFetchMode(PDO::FETCH_ASSOC);
+                return Instructor($stmt->fetch());
+        }
+
+}
+?>
+
