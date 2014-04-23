67413f3008ae667031750d9eb1d31c744e3e2014
Dmitri Ostapenko <dostapen@u.rochester.edu>
2014-04-23 00:50:39 +0000

    creating a php which looks up TAs for 173

--- ta-list-for-course.php
+++ ta-list-for-course.php
@@ -1,16 +1,15 @@
 <?php
 require_once('../dbsetup.php');
 require_once('utils.php');
+function getTas() {
 
- public function applyCourse($crn,$forCredit) {
-        Utils::getVoid('INSERT INTO applications (crn,ta_id,time_signup,time_response,state,for_credit) VALUES (:crn,:netid,:signup,:response,:state,:credit)',
-            array(':coursecrn' => $crn,
-            ':netid' => $this->netid,
-            ':signup' => $_SERVER['REQUEST_TIME'],
-            ':response' => NULL,
-            ':state' => 'pending',
-            ':credit' => $forCredit));
-        update();
+ $stmt = $db->prepare('SELECT netid,name,email,class_year FROM courses c
+						INNER JOIN course_apps ca ON c.crn = ca.crn
+						INNER JOIN tas t ca ON t.netid = ca.netid
+						WHERE ca.state = "approved" AND c.year = 2014 AND c.semester = "fall"
+						AND c.department = "CSC" AND course_number = 173;',
+            array(),
+            function ($x) { return new TA($x); });
     }
 ?>
 <!DOCTYPE html>
@@ -19,8 +18,17 @@ require_once('utils.php');
 <body>
 <h1>TuesdayNight on Betaweb</h1>
 <h2>TA Lister</h2>
+<?php
+ echo '<table cellspacing="1"><thead><th width="150">Net ID</th><th width="150">Name</th><th width="250">E-Mail</th><th>Class Year</th></thead><tbody>';
+$tas = getTas();
+    foreach ($tas as $ta) {
+        echo "<tr><td>{$ta->getNetID()}</td><td>{$ta->getName()}</td><td>{$ta->getEmail()}</td><td>{$ta->getClassYear()}</td></tr>";
+    }
+    echo '</tbody></table>';
+
+?>
 
 
-
-
+</body>
+</html>
 
