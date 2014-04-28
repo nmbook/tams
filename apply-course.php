5bd47e125fea1ea7ea25762618df3c645b9ea47b
Tyler Hannan <thannan@novetta.com>
2014-04-28 23:20:20 +0000

    restructure apply-course

--- apply-course.php
+++ apply-course.php
@@ -7,24 +7,18 @@
 <?php
 	require_once('course.php');
 	require_once('ta.php');
-	if (!isset($_GET['netid']) || !isset($_GET['for_credit'])) {
+	if (!isset($_GET['netid']) || !isset($_GET['for_credit']) ||
+		(!isset($_GET['crn']) && !(isset($_GET['dept']) && isset($_GET['number']) && $isset($_GET['year']) && isset($_GET['semester'])))) {
 ?>
 	<form action = "apply-course.php" method="get">
 	Netid: <input type="text" name="netid"><br>
 	For Credit?: <input type"text" name="for_credit"><br>
+	Course Department: <input type="text" name="dept"><br>
+	Course Number: <input type="text" name="number"><br>
+	Year: <input type="text" name="year"><br>
+	Semester: <input type="text" name="semester"><br>
 	<input type="submit">
 	</form>
-<?php
-	}
-	elseif (!isset($_GET['crn']) && !(isset($_GET['dept']) && isset($_GET['number']) && $isset($_GET['year']) && isset($_GET['semester']))) {
-?>
-<form action="apply-course.php" method="get">
-Course Department: <input type="text" name="dept"><br>
-Course Number: <input type="text" name="number"><br>
-Year: <input type="text" name="year"><br>
-Semester: <input type="text" name="semester"><br>
-<input type="submit">
-</form>
 <?php
 	}
 	else {
