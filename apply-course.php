cb0bada20bacec25202eed8bc5bdf935b3185a91
Tyler Hannan <thannan@novetta.com>
2014-04-29 00:40:26 +0000

    fixed error in apply-course

--- apply-course.php
+++ apply-course.php
@@ -7,16 +7,16 @@
 <?php
 	require_once('course.php');
 	require_once('ta.php');
-	if (!isset($_GET['netid']) || !isset($_GET['for_credit']) ||
-		(!isset($_GET['crn']) && !(isset($_GET['dept']) && isset($_GET['number']) && $isset($_GET['year']) && isset($_GET['semester'])))) {
+	if (!isset($_GET['netid']) || !isset($_GET['for_credit']) || 
+		(!isset($_GET['crn']) && !(isset($_GET['dept']) && isset($_GET['number']) && isset($_GET['year']) && isset($_GET['semester'])))) {
 ?>
 	<form action = "apply-course.php" method="get">
-	Netid: <input type="text" name="netid"><br>
-	For Credit?: <input type"text" name="for_credit"><br>
-	Course Department: <input type="text" name="dept"><br>
-	Course Number: <input type="text" name="number"><br>
-	Year: <input type="text" name="year"><br>
-	Semester: <input type="text" name="semester"><br>
+	Netid: <input type="text" name="netid"/><br>
+	For Credit?: <input type"text" name="for_credit"/><br>
+	Course Department: <input type="text" name="dept"/><br>
+	Course Number: <input type="text" name="number"/><br>
+	Year: <input type="text" name="year"/><br>
+	Semester: <input type="text" name="semester"/><br>
 	<input type="submit">
 	</form>
 <?php
@@ -26,7 +26,7 @@
 		$netid = $_GET['netid'];
 		$for_credit = $_GET['for_credit'];
 		echo "break 2<br>\n";
-		/*try {
+		try {
 			$ta = TA::getByNetID($netid);
 		}
 		catch (Exception $e) {
@@ -52,7 +52,7 @@
 		}
 		catch (Exception $e) {
 			echo 'Caught Exception3: ', $e->getMessage(), "\n";	
-		}*/
+		}
 		echo "break 7<br>\n";
 	}
 ?>
