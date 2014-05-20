de2a753991ca5e1f341d0fed183ae5a5d2acdb63
Nate Book <nmbook@gmail.com>
2014-05-20 13:48:22 -0400

    Moving file names to more consisant names.

--- betaweb-pull.sh
+++ betaweb-pull.sh
@@ -2,15 +2,15 @@
 
 # full update for betaweb
 git pull
-chmod go+rx data design
+chmod go+rx data design seeder.py betaweb-pull.sh
 chmod go+r * data/* design/*
 
 # old filename
-ln -fs ta-list.php talist-tuesdaynight.php
+#ln -fs ta-list.php talist-tuesdaynight.php
 
 # view source of all .php files
-for FILE in *.php
-do
-    ln -fs $FILE `echo $FILE | sed 's/\(.*\.\)php/\1txt/'`
-done
+#for FILE in *.php
+#do
+#    ln -fs $FILE `echo $FILE | sed 's/\(.*\.\)php/\1txt/'`
+#done
 
